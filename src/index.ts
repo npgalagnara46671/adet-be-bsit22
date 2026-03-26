9307cbaa36dc7aa8bf404d73975bea916b107491
Nicholai Patrick C. Galagnara <npgalagnara46671@liceo.edu.ph>
2026-03-26 15:13:28 +0800

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
+import postRoutes from './routes/post.routes'  
+
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
+
+app.route('/posts', postRoutes);
 
 serve({
   fetch: app.fetch,
